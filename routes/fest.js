c7bbd85d4d7d6f011b53e72b0c703c6384496502
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-03 00:14:26 +0530

    moved some routes >>/ongoingevents >>/adduser >>/removeuser >>/removeexuser

--- routes/fest.js
+++ routes/fest.js
@@ -4,21 +4,13 @@ const {
   register,
   unregister,
   getEvents,
-  ongoingEvents,
-  addUser,
-  removeUser,
-  removeExternalUser,
 } = require("../controllers/fest");
 const { verifyToken } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.get("/getlist", getList);
+router.get("/getlist", verifyToken, getList);
 router.post("/register", verifyToken, register);
 router.post("/unregister", verifyToken, unregister);
 router.post("/getevents", verifyToken, getEvents);
-router.get("/ongoingevents", ongoingEvents);
-router.post("/adduser", verifyToken, addUser);
-router.post("/removeuser", verifyToken, removeUser);
-router.post("/removeexuser", verifyToken, removeExternalUser);
 
 module.exports = router;
