eedd84b493e65d828281446c5dea46e445ee16b8
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-03 00:10:20 +0530

    created middleware to authenticate admin

---
+++ middlewares/adminMiddleware.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const client = require("../configs/database");
+
+exports.verifyAdmin = (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (token) {
+        jwt.verify(token, "" + process.env.SECRET_KEY, async (err, decoded) => {
+            if (err)
+                return res.status(403).json({
+                    error: "Invalid token!",
+                });
+
+            try {
+                const userEmail = decoded.email;
+                const userId = decoded.userId;
+
+                const data = await client
+                    .query(`SELECT * FROM users where user_id = '${userId}'`);
+
+                if (data.rows.length == 0) {
+                    res.status(400).json({
+                        message: "Invalid email or password!",
+                    });
+                } else {
+                    if (!(data.rows[0].is_admin)) return;
+                    req.email = userEmail;
+                    req.userId = userId;
+                    next();
+                }
+
+            } catch (err1) {
+                res.status(500).json({
+                    message: `${err1}`,
+                });
+            }
+
+        });
+    }
+};
