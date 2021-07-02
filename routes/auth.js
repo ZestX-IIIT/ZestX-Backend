b51cc7f4b897e1e54a5db261e9381b102e378068
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-03 00:12:33 +0530

    added two routes >>/forgotpasswordsignin >>/forgotpasswordhomepage

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const {signUp, signIn} = require("../controllers/auth");
+const { signUp, signIn, forgotPasswordForSignIn, forgotPasswordForHomepage } = require("../controllers/auth");
 
-router.post("/signup",signUp);
-router.post("/signin",signIn);
+router.post("/signup", signUp);
+router.post("/signin", signIn);
+router.post("/forgotpasswordsignin", forgotPasswordForSignIn);
+router.get("/forgotpasswordhomepage", verifyToken, forgotPasswordForHomepage);
 
 module.exports = router;
