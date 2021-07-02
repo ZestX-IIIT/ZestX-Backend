2e629615d2f27fbb341c1661d0db904320253298
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-03 00:10:43 +0530

    added async await

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,33 +4,35 @@ const client = require("../configs/database");
 exports.verifyToken = (req, res, next) => {
   const token = req.headers.authorization;
 
-  jwt.verify(token, "" + process.env.SECRET_KEY, (err, decoded) => {
-    if (err) {
-      res.status(500).json({
-        error: `${err}`,
-      });
-    } else {
-      const userEmail = decoded.email;
-      const userId = decoded.userId;
-
-      client
-        .query(`SELECT *FROM users where user_id = '${userId}'`)
-        .then((data) => {
-          if (data.rows.length == 0) {
-            res.status(400).json({
-              message: "User does not exist",
-            });
-          } else {
-            req.email = userEmail;
-            req.userId = userId;
-            next();
-          }
-        })
-        .catch((err1) => {
-          res.status(500).json({
-            message: `${err1}`,
-          });
+  if (token) {
+    jwt.verify(token, "" + process.env.SECRET_KEY, async (err, decoded) => {
+      if (err)
+        return res.status(403).json({
+          error: "Invalid token!",
         });
-    }
-  });
+
+      try {
+        const userEmail = decoded.email;
+        const userId = decoded.userId;
+
+        const data = await client
+          .query(`SELECT * FROM users where user_id = '${userId}'`);
+
+        if (data.rows.length == 0) {
+          res.status(400).json({
+            message: "Invalid email or password",
+          });
+        } else {
+          req.email = userEmail;
+          req.userId = userId;
+          next();
+        }
+
+      } catch (err1) {
+        res.status(500).json({
+          message: `${err1}`,
+        });
+      }
+    });
+  }
 };
