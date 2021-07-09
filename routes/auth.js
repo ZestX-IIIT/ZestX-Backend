a69738bf391eda21327b0054787268a57142ac19
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-09 19:40:07 +0530

    changed mail templates

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const { signUp, signIn, forgotPasswordForSignIn } = require("../controllers/auth");
-const { verifyToken } = require("../middlewares/authMiddleware");
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
